ec552c69b58fb63d1ac69b76fc17baeaed130277
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-10-07 11:13:22 +0200

    Add serviceWorker

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,12 @@
 /* eslint-disable no-restricted-globals */
 var cacheName = 'manchester-united-store';
 var filesToCache = [
-  '../build/static/css/main.39702bea.chunk.css',
-  '../build/static/js/2.348a7a9b.chunk.js',
-  '../build/static/js/main.743d416d.chunk.js',
-  '../build/static/js/runtime-main.b9c013c5.js',
-  '../build/static/images/6s_ss4.jpg',
-  '../build/static/images/alex-ferguson-my-autobiogrpahy.jpg',
-  '../build/static/images/bobby-charlton-autobiography.jpg',
-  '../build/static/images/class-of-92.jpg',
-  '../build/static/images/george-best-blessed.jpg',
-  '../build/static/images/james-leighton-duncan-edwards.jpg',
-  '../build/static/images/manchester-united-all-over-print-t-shirt-red-baby.jpg',
-  '../build/static/images/manchester-united-away-goalkeeper-shirt-2020-21.jpg',
-  '../build/static/images/manchester-united-away-kits.png',
-  '../build/static/images/manchester-united-away-shirt-2020-21.jpg',
-  '../build/static/images/manchester-united-ball-keyring-silver.jpg',
-  '../build/static/images/manchester-united-heritage-football-size-5.jpg',
-  '../build/static/images/manchester-united-home-goalkeeper-shirt-2020-21.jpg',
-  '../build/static/images/manchester-united-home-kits.png',
-  '../build/static/images/manchester-united-home-shirt-2020-21.jpg',
-  '../build/static/images/manchester-united-new-era-fred-the-red.jpg',
-  '../build/static/images/manchester-united-silcone-watch-red-kids.jpg',
-  '../build/static/images/manchester-united-strap-keyring.jpg',
-  '../build/static/images/manchester-united-third-kits.png',
-  '../build/static/images/manchester-united-third-shirt-2020-21.jpg',
-  '../build/static/images/manchester-united-x-paul-smith-mens-red-and-grey-stripe-rectangle-cufflinks.jpg',
-  '../build/static/images/manchester-united-x-paul-smith-mens-vintage-rosette-print-interior-billfold-wallet.jpg',
+  '/index.html',
+  '/manifest.json',
+  '/static/css/*',
+  '/static/js/*',
+  '/images/*',
+  '/icons/*',
 ];
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
